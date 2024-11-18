aa61274d4fe50bfbf20a4936f61ba70a5a0f3828
Andrew02021981 <andrejsazonov5487@gmail.com>
2024-11-18 19:11:09 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -10,41 +10,71 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        if isinstance(other, (int, House)):
+        if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, (int, float)):
+            return self.number_of_floors == other
+        else:
+            raise ValueError("Операция невозможна. Атрибут other должен быть типом int, float или объектом House")
 
     def __lt__(self, other):
-        if isinstance(other, (int, House)):
+        if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, (int, float)):
+            return self.number_of_floors < other
+        else:
+            raise ValueError("Операция невозможна. Атрибут other должен быть типом int, float или объектом House")
 
     def __le__(self, other):
-        if isinstance(other, (int, House)):
+        if isinstance(other, House):
             return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, (int, float)):
+            return self.number_of_floors <= other
+        else:
+            raise ValueError("Операция невозможна. Атрибут other должен быть типом int, float или объектом House")
 
     def __gt__(self, other):
-        if isinstance(other, (int, House)):
+        if isinstance(other, House):
             return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, (int, float)):
+            return self.number_of_floors > other
+        else:
+            raise ValueError("Операция невозможна. Атрибут other должен быть типом int, float или объектом House")
 
     def __ge__(self, other):
-        if isinstance(other, (int, House)):
+        if isinstance(other, House):
             return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, (int, float)):
+            return self.number_of_floors >= other
+        else:
+            raise ValueError("Операция невозможна. Атрибут other должен быть типом int, float или объектом House")
 
     def __ne__(self, other):
-        if isinstance(other, (int, House)):
+        if isinstance(other, House):
             return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, (int, float)):
+            return self.number_of_floors != other
+        else:
+            raise ValueError("Операция невозможна. Атрибут other должен быть типом int, float или объектом House")
 
     def __add__(self, value):
-        if isinstance(value, (int, House)):
+        if isinstance(value, (int, float, House)):
             self.number_of_floors += value
             return self
+        else:
+            raise ValueError("Операция невозможна. Атрибут value должен быть типом int, float или объектом House")
 
     def __iadd__(self, value):
-        if isinstance(value, (int, House)):
+        if isinstance(value, (int, float, House)):
             return self + value
+        else:
+            raise ValueError("Операция невозможна. Атрибут value должен быть типом int, float или объектом House")
 
     def __radd__(self, value):
-        if isinstance(value, (int, House)):
+        if isinstance(value, (int, float, House)):
             return self + value
+        else:
+            raise ValueError("Операция невозможна. Атрибут value должен быть типом int, float или объектом House")
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
@@ -69,4 +99,3 @@ print(h1 >= h2) # __ge__
 print(h1 < h2) # __lt__
 print(h1 <= h2) # __le__
 print(h1 != h2) # __ne__
-
